4ab29c3343eed5db7520b80efead0c72494b82c6
ryotaro-rks <ryotaro.seya@rakus.co.jp>
2021-05-20 14:10:03 +0900

    Carクラスが完成しました
    Carクラスを作成
    
    create speed field
    
    add speed field

---
+++ src/main/java/com/example/domain/Car.java
@@ -0,0 +1,23 @@
+package com.example.domain;
+
+public class Car {
+	private int speed;
+	private String name;
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
